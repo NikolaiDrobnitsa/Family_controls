dfc8832669fd07e9779154f0c147e36f7a95aa39
User PRO <User PRO@COMPUTER>
2022-07-23 16:19:24 +0300

    Добавьте файлы проекта.

---
+++ Family_controls/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Family_controls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.AllApplistBox = new System.Windows.Forms.ListBox();
+            this.BlocklistBox = new System.Windows.Forms.ListBox();
+            this.TempBlocklistBox = new System.Windows.Forms.ListBox();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripComboBox1,
+            this.toolStripButton4,
+            this.toolStripTextBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(840, 28);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(139, 25);
+            this.toolStripButton1.Text = "Заблокировать";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(146, 25);
+            this.toolStripButton2.Text = "Разблокировать";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(143, 25);
+            this.toolStripButton3.Text = "toolStripButton3";
+            // 
+            // AllApplistBox
+            // 
+            this.AllApplistBox.FormattingEnabled = true;
+            this.AllApplistBox.ItemHeight = 16;
+            this.AllApplistBox.Location = new System.Drawing.Point(4, 29);
+            this.AllApplistBox.Name = "AllApplistBox";
+            this.AllApplistBox.Size = new System.Drawing.Size(160, 292);
+            this.AllApplistBox.TabIndex = 1;
+            // 
+            // BlocklistBox
+            // 
+            this.BlocklistBox.FormattingEnabled = true;
+            this.BlocklistBox.ItemHeight = 16;
+            this.BlocklistBox.Location = new System.Drawing.Point(164, 29);
+            this.BlocklistBox.Name = "BlocklistBox";
+            this.BlocklistBox.Size = new System.Drawing.Size(168, 292);
+            this.BlocklistBox.TabIndex = 2;
+            // 
+            // TempBlocklistBox
+            // 
+            this.TempBlocklistBox.FormattingEnabled = true;
+            this.TempBlocklistBox.ItemHeight = 16;
+            this.TempBlocklistBox.Location = new System.Drawing.Point(334, 29);
+            this.TempBlocklistBox.Name = "TempBlocklistBox";
+            this.TempBlocklistBox.Size = new System.Drawing.Size(167, 292);
+            this.TempBlocklistBox.TabIndex = 3;
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "5 мин",
+            "10 мин",
+            "15 мин",
+            "20 мин",
+            "25 мин",
+            "30 мин"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 28);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(143, 25);
+            this.toolStripButton4.Text = "toolStripButton4";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 28);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 450);
+            this.Controls.Add(this.TempBlocklistBox);
+            this.Controls.Add(this.BlocklistBox);
+            this.Controls.Add(this.AllApplistBox);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ListBox AllApplistBox;
+        private System.Windows.Forms.ListBox BlocklistBox;
+        private System.Windows.Forms.ListBox TempBlocklistBox;
+    }
+}
+
